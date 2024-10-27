24edb22e279375afbc55aed3c0c21101fb89ae39
Md.Rafiul <mrafiul.alam7@gmail.com>
2024-10-27 19:29:56 +0600

    Updated

---
+++ Day 9/array.js
@@ -0,0 +1,53 @@
+let array 
+array = [10,12,14,16]
+
+let a = "string"
+
+// console.log(array.length)
+// console.log(array[0])
+// console.log(array[1])
+// console.log(array[2])
+// console.log(array[3])
+
+
+// for(var i = 0;i<=array.length; i++){
+//     console.log( i ,"=", array[i])
+// // }
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.toString())
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let fruit = fruits.at(2);
+// console.log(fruit)
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.join(" | "))
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits)
+// fruits.push("Dragon");
+// console.log(fruits)
+// console.log(fruits.pop())
+// console.log(fruits)
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.shift();
+// console.log(fruits)
+// fruits.unshift("Lemon");
+// console.log(fruits)
+
+// const myGirls = ["Cecilie", "Lone"];
+// const myBoys = ["Emil", "Tobias", "Linus"];
+
+// const myChildren = myGirls.concat(myBoys);
+// console.log(myChildren)
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const newFruits = [...fruits, "Dragon", "MAlta"]
+
+console.log(newFruits)
+console.log(fruits)
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const citrus = fruits.slice(2);
+// console.log(citrus)
+
---
+++ Day 9/string.js
@@ -0,0 +1,32 @@
+// let text = "HELLO WORLD";
+// let char = text.charAt(0);
+
+// console.log(char)
+
+// let text = "A HELLO WORLD";
+// let char = text.charCodeAt(0);
+
+// console.log(char)
+// const name = "W3Schools";
+// let letter = name.at(2);
+// console.log(letter)
+
+// let text = "HELLO WORLD";
+// let char = text[0];
+// console.log(char)
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(7, 13);
+// console.log(part)
+
+// let str = "Apple, Banana, Kiwi";
+// let part = str.substring(7, 13);
+// console.log(part)
+
+// let str = "Apple, Banana, Kiwi";
+// let part = str.substr(7, 6);
+// console.log(part)
+
+let str = "Apple, Banana, Kiwi";
+let part = str.substr(-8);
+console.log(part)
