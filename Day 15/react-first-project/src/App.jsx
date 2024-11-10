269f102170b742f95b90a4b91da482386e73e642
Md.Rafiul <mrafiul.alam7@gmail.com>
2024-11-10 19:10:29 +0600

    updated

---
+++ Day 15/react-first-project/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+function App() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#home">About</a></li>
+            <li><a href="#home">Contact</a></li>
+            <li><a href="#home">Blog</a></li>
+            
+          </ul>
+          <ul>
+          <li><a href="#home">Sign Up</a></li>
+          <li><a href="#home">Sign In</a></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default App
